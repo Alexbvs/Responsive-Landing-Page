6d2aeee36ea84a9991b0fb7cc69d4acd16db7630
Artem <oleksienkoartem@gmail.com>
2020-05-28 21:46:58 +0300

    Add service carousel

--- app/js/app.js
+++ app/js/app.js
@@ -14,4 +14,39 @@ $(function() {
 	}).bind('close:finish', function(){
 		$('.hamburger').removeClass('is-active');
 	})
+
+	// owlCarousel initialized for correct height calculation
+	$('.carousel-services').on('initialized.owl.carousel', function(){
+		setTimeout(function(){
+			carouselService()
+		}, 100);
+	});
+
+	// owlCarousel start
+	$('.carousel-services').owlCarousel({
+		loop: true,
+		nav: true,
+		dots: false,
+		smartSpeed: 700,
+		navText: ['<i class="fas fa-angle-double-left"></i>' , '<i class="fas fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			}, 800: {
+				items: 2
+			}, 1100: {
+				itens: 3
+			}
+		}
+	});
+
+	// owlCarousel height calculation
+	function carouselService() {
+		$('.carousel-services-item').each (function() {
+			let ths = $(this),
+				thsh = ths.find('.carousel-services-composition').outerHeight();
+				ths.find('.carousel-services-image').css('min-height', thsh);
+		});
+	} carouselService();
 })
